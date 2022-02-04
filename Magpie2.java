311ff56cf1ff1e9779d8afcd2cda0ec16a508b3f
Thomas Boston (Class of 2022) <220195tb@internal.salesianum.org>
2022-02-04 10:06:51 -0500

    cb

--- Magpie2.java
+++ Magpie2.java
@@ -9,16 +9,35 @@ public class Magpie2 {
      String response = "";
      if (statement.indexOf("no") >= 0) {
        response = "Why so negative?";
-     } else if (statement.indexOf("mother") >= 0
+      } else if (statement.indexOf("mother") >= 0
                  || statement.indexOf("father") >= 0
                  || statement.indexOf("sister") >= 0
                  || statement.indexOf("brother") >= 0) {
        response = "Tell me more about your family.";
-     } else {
+            
+      } else if (statement.indexOf("cat") >=0 
+                || statement.indexOf("dog") >= 0
+                || statement.indexOf("snake") >= 0) {
+        response = "Tell me more about your pets.";
+                
+      } if (statement.indexOf("hotdog") >= 0
+            || statement.indexOf("burger") >= 0
+            || statement.indexOf("salad") >= 0) {
+        response = "My favourite food is banans.";
+      } else {
        response = getRandomResponse();
      }
      return response;
    }
+
+
+
+  
+
+    /**
+       * Pick a default response to use if nothing else fits.
+       * @return a non-committal string
+       */
 
    private String getRandomResponse()
    {
@@ -44,12 +63,12 @@ public class Magpie2 {
      Magpie2 maggie = new Magpie2();
 
      System.out.println(maggie.getGreeting());
-     System.out.println(">My mother and I talked last night.");
-     System.out.println(maggie.getResponse("My mother and I talked last night."));
-     System.out.println(">I said no.");
-     System.out.println(maggie.getResponse("I said no!"));
-     System.out.println(">The weather is nice.");
-     System.out.println(maggie.getResponse("The weather is nice."));
+     System.out.println(">I am friends with my bother");
+     System.out.println(maggie.getResponse("I am friends with my brother"));
+     System.out.println(">My favourite food is hotdog");
+     System.out.println(maggie.getResponse("My favourte food is hotdog"));
+     System.out.println(">The weather bad bruh");
+     System.out.println(maggie.getResponse("It's raining cats and dogs"));
      System.out.println(">Do you know my brother?");
      System.out.println(maggie.getResponse("Do you know my brother?"));
    }
