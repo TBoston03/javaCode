f0cdf7768ec5695dec5dda8a88e52e9338cdc0f7
Thomas Boston (Class of 2022) <220195tb@internal.salesianum.org>
2022-03-17 14:59:40 -0400

    cy

--- Cypher/src/App.java
+++ Cypher/src/App.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner keyboardEntry = new Scanner(System.in);
+        System.out.print("Enter a word:");
+        String entry = keyboardEntry.nextLine();
+        String[][] generic = new String[entry.length()/2][2];
+        
+        }
+        
+        
+        
+        keyboardEntry.close();
     }
 }
+/*String select = generic[i][h];
+                System.out.print(select + " ");for(int g = 0; g < entry.length(); g++){
+          for(int i = 0; i < generic.length; i ++){
+            System.out.print("");
+            for(int h = 0; h < generic[i].length; h ++){
+               System.out.print(generic[i][h]);
+            }
+        }  */
